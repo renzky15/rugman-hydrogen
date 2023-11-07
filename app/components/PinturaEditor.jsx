26fb9d4c8011ef6a180bb1dc2bbe88a0248f245d
renzpoogi <98722611+renzpoogi@users.noreply.github.com>
2023-11-07 23:43:49 +0800

    Update PinturaEditor.jsx

--- app/components/PinturaEditor.jsx
+++ app/components/PinturaEditor.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 // react-pintura
 import { PinturaEditor } from "@pqina/react-pintura";
 
-import { getEditorDefaults } from "@pqina/pintura";
+import { createNode, getEditorDefaults } from "@pqina/pintura";
 
 // get default properties
 const editorDefaults = getEditorDefaults({
@@ -121,7 +121,7 @@ const editorDefaults = getEditorDefaults({
     targetSize: {
       width: 1024,
       height: 768,
-      fit: 'contain'
+      fit: 'cover'
     },
     postprocessImageData: (imageData) =>
       new Promise((resolve, reject) => {
@@ -144,12 +144,12 @@ const editorDefaults = getEditorDefaults({
             watermark,
 
             // the watermark x and y position
-            350,
-            140,
+            100,
+            100,
 
             // the watermark width and height
-            400,
-            400
+            500,
+            500
           );
 
           // Get and return the modified imageData
@@ -187,22 +187,106 @@ const downloadFile = (file) => {
   }, 0);
 }
 
+const browse = (options) => {
+  return new Promise((resolve) => {
+    const element = document.createElement('input');
+    element.type = 'file';
+    element.accept = options.accept;
+    element.onchange = () => {
+      const [file] = element.files;
+      if (!file) return resolve();
+      resolve(file);
+    };
+    element.click();
+  });
+};
+
 export default function PinturaEditorComponent() {
   // inline result
   const editorRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState(null);
+
+  useEffect(() => {
+    const blankImage = document.createElement('canvas');
+    blankImage.width = 1024;
+    blankImage.height = 768;
+
+    // The default <canvas> is transparent, let's make it white
+    const imageContext = blankImage.getContext('2d');
+    imageContext.fillStyle = 'white';
+    imageContext.fillRect(0, 0, blankImage.width, blankImage.height);
+
+    // Set editor source to the canvas
+    setImageSrc(blankImage);
+  }, []);
+
 
   const handleProcess = (imageState) => {
     downloadFile(imageState.dest);
   }
+  const handleSelectImage = (event) => {
+    const file = event.target.files[0];
+    setImageSrc(file);
+  }
+  const handleEditorUpdateshape = (shape) => {
+    console.log('updateshape', shape);
+  };
+  const beforeUpdateShape = (shape, props, context) => {
+    console.log('beforeUpdateShape', shape, props, context);
+    return props;
+  };
+
+  const willRenderShapeControls = (controls, selectedShapeId) => {
+    controls[0][3].push(
+      // Add a "Select image" button
+      createNode('Button', 'my-button', {
+        label: 'Select image',
+        onclick: async () => {
+          // Find the currently selected shape
+          const annotations =
+            editorRef.current.editor.imageAnnotation;
+          const shape = annotations.find(
+            (shape) => shape.id === selectedShapeId
+          );
+
+          // browse for an image
+          const file = await browse({
+            accept: 'image/*',
+          });
+
+          // no file selected
+          if (!file) return;
+
+          // update background image
+          shape.backgroundImage = URL.createObjectURL(file);
+
+          // redraw annotations
+          editorRef.current.editor.imageAnnotation = annotations;
+        },
+      })
+    );
+    return controls;
+  };
+
   return (
-    <div>
-      <div style={{ height: '100vh', width: '100%' }}>
+    <div className="flex flex-col items-center">
+      <div className="relative" style={{ height: '100vh', width: '100%' }}>
+        <div className="text-black absolute right-20 top-4 z-10">
+          <button htmlFor="file-upload" className="text-black bg-[#FFD742] text-[14px] px-2 py-1 rounded-full">
+            Upload image
+          </button>
+          <input id="file-upload" className="hidden" onChange={handleSelectImage} type="file" />
+        </div>
+        {/*<input type="file" className="bg-transparent "  onChange={handleSelectImage} name="Select image"/>*/}
         <PinturaEditor
           {...editorDefaults}
           ref={editorRef}
-          src={"./image.jpg"}
-          imageCropAspectRatio={1}
+          src={imageSrc}
           onLoad={(res) => console.log("load image", res)}
+          onUpdateshape={handleEditorUpdateshape}
+          // onSelectshape={handleEditorSelectshape}
+          // beforeUpdateShape={beforeUpdateShape}
+          willRenderShapeControls={willRenderShapeControls}
           onProcess={handleProcess}
           imageAnnotation={[{
             x: 0,
@@ -212,7 +296,18 @@ export default function PinturaEditorComponent() {
             backgroundImage: 'img.png',
             alwaysOnTop:true,
             backgroundSize:'contain'
-          }
+          },
+            {
+              id:'beforeImage',
+              x: 243,
+              y: 480,
+              width: 150,
+              height: 165,
+              backgroundColor: [0, 0, 0],
+              rotation: -0.16718109997479447,
+              backgroundImage: '',
+              alwaysOnTop:true,
+            },
           ]}
         />
       </div>
